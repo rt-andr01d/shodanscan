053d61e3aee25245fe95af09b9b0ebaeacc4dfe4
rt-andr01d <rtandr01d@protonmail.com>
2020-01-21 22:57:08 -0600

    Rest API Implemented

--- shodanscan.py
+++ shodanscan.py
@@ -4,20 +4,44 @@ import requests
 import json
 import os
 import signal
+import argparse
+import shodan
+
+header = ''' 
+███████╗██╗  ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗███████╗ ██████╗ █████╗ ███╗   ██╗
+██╔════╝██║  ██║██╔═══██╗██╔══██╗██╔══██╗████╗  ██║██╔════╝██╔════╝██╔══██╗████╗  ██║
+███████╗███████║██║   ██║██║  ██║███████║██╔██╗ ██║███████╗██║     ███████║██╔██╗ ██║
+╚════██║██╔══██║██║   ██║██║  ██║██╔══██║██║╚██╗██║╚════██║██║     ██╔══██║██║╚██╗██║
+███████║██║  ██║╚██████╔╝██████╔╝██║  ██║██║ ╚████║███████║╚██████╗██║  ██║██║ ╚████║
+╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝
+'''
+
+parser = argparse.ArgumentParser(description='Search a list of IP addresses across Shodan.io using the restful api')
+parser.add_argument('-f', '--file', help='File containing list of IP addresses to search', required=True)
+parser.add_argument('-a', '--api', help='API key file', required=True)
+parser.add_argument('-o', '--outfile', help='File to write output to')
+args = parser.parse_args()
 
 
-file_in = input("IP file: ")
-ip_file = open(file_in, "r")
-api_file = open("api.txt", "r")
-api = api_file.readline().rstrip('\n')
 
-def loop():
+print(header)
+ip_file = open(args.file, "r")
+api_file = open(args.api, "r")
+SHODAN_API_KEY = api_file.readline().rstrip('\n')
+api = shodan.Shodan(SHODAN_API_KEY)
+
+def search():
   for ip in ip_file:
-    url = "https://api.shodan.io/shodan/host/" + ip + "?key=" + api
+    url = "https://api.shodan.io/shodan/host/" + ip + "?key=" + SHODAN_API_KEY
     request = requests.get(url)
     txt = request.text
     print("\n" + ip )
     parsed = json.loads(txt)
-    print(json.dumps(parsed, indent=2, sort_keys=True))
+    if args.outfile:
+      outfile = open(args.outfile, "w")
+      output = json.dumps(parsed, indent=2, sort_keys=True)
+      outfile.write(output)
+    else:
+      print(json.dumps(parsed, indent=2, sort_keys=True))
 
-loop()
+search()
